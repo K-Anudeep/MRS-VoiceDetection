942461eb487413618010ffacb6f5176f91aaa8da
Anudeep <anudeep2520@gmail.com>
2018-06-13 00:03:37 +0530

    PyScript for GCS Transcription
    This is a cutdown version of the full script which allows only GCS file transcription as it is more efficient.

---
+++ Transcribe.py
@@ -0,0 +1,41 @@
+import argparse
+import io
+import sys
+
+# [START def_transcribe_gcs]
+def transcribe_gcs(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    audio = types.RecognitionAudio(uri=gcs_uri)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code='te-IN')
+
+    operation = client.long_running_recognize(config, audio)
+
+    print('Waiting for operation to complete...')
+    response = operation.result(timeout=9999999999)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        sys.stdout = open("Documents/Intern/telugu.txt", "a")
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+    print("Transcript Done.")
+# [END def_transcribe_gcs]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'path', help='File or GCS path for audio file to be recognized')
+    args = parser.parse_args()
+    if args.path.startswith('gs://'):
+        transcribe_gcs(args.path)
